57aa8cef487338fa65a4a7159f0aa80f1adf6612
Brahvim <brahvimombhaktvatsal@gmail.com>
2023-11-08 08:57:57 +0530

    ...Formatting improvements.

--- include/statically_allocated/static_heap_array.h
+++ include/statically_allocated/static_heap_array.h
@@ -12,70 +12,70 @@ struct type##_static_heap_array {																																		\
 };																																										\
 																																										\
 /**  */								 																																	\
-bool type##_static_heap_array_create(struct type##_static_heap_array *array, size_t element_size, size_t initial_capacity);											\
+bool type##_static_heap_array_create(struct type##_static_heap_array *array, size_t element_size, size_t initial_capacity);												\
 																																										\
 /**  */								 																																	\
-bool type##_static_heap_array_insert(struct type##_static_heap_array *array, const type *element);																	\
+bool type##_static_heap_array_insert(struct type##_static_heap_array *array, const type *element);																		\
 																																										\
 /**  */								 																																	\
-type type##_static_heap_array_remove(struct type##_static_heap_array *array, const type *element);																	\
+type type##_static_heap_array_remove(struct type##_static_heap_array *array, const type *element);																		\
 																																										\
 /**  */								 																																	\
-type type##_static_heap_array_consume(struct type##_static_heap_array *array, const type *element);																	\
+type type##_static_heap_array_consume(struct type##_static_heap_array *array, const type *element);																		\
 																																										\
 /**  */								 																																	\
-type type##_static_heap_array_supply(struct type##_static_heap_array *array, const type *element);																	\
+type type##_static_heap_array_supply(struct type##_static_heap_array *array, const type *element);																		\
 																																										\
 /**  */								 																																	\
-bool type##_static_heap_array_resize(struct type##_static_heap_array *array, size_t new_capacity);																	\
+bool type##_static_heap_array_resize(struct type##_static_heap_array *array, size_t new_capacity);																		\
 																																										\
 /**  */								 																																	\
-bool type##_static_heap_array_free(struct type##_static_heap_array *array);																							\
+bool type##_static_heap_array_free(struct type##_static_heap_array *array);																								\
 																																										\
 /**  */								 																																	\
-type type##_static_heap_array_push(struct type##_static_heap_array *array); 																						\
+type type##_static_heap_array_push(struct type##_static_heap_array *array); 																							\
 																																										\
 /**  */								 																																	\
-struct type##_static_heap_array* type##_static_heap_array_pop(struct type##_static_heap_array *array); 															\
+struct type##_static_heap_array* type##_static_heap_array_pop(struct type##_static_heap_array *array); 																	\
 																																										\
 /**  */								 																																	\
-struct type##_static_heap_array* type##_static_heap_array_clear(struct type##_static_heap_array *array); 															\
+struct type##_static_heap_array* type##_static_heap_array_clear(struct type##_static_heap_array *array); 																\
 																																										\
 /**  */								 																																	\
-bool type##_static_heap_array_empty(struct type##_static_heap_array *array); 																						\
+bool type##_static_heap_array_empty(struct type##_static_heap_array *array); 																							\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_size(struct type##_static_heap_array *array); 																						\
+size_t type##_static_heap_array_size(struct type##_static_heap_array *array); 																							\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_capacity(struct type##_static_heap_array *array); 																					\
+size_t type##_static_heap_array_capacity(struct type##_static_heap_array *array); 																						\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_reserve(struct type##_static_heap_array *array); 																					\
+size_t type##_static_heap_array_reserve(struct type##_static_heap_array *array); 																						\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_trim(struct type##_static_heap_array *array); 																						\
+size_t type##_static_heap_array_trim(struct type##_static_heap_array *array); 																							\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_sort(struct type##_static_heap_array *array, bool (*comparison_function)(type *current_element, type *other_element));				\
+size_t type##_static_heap_array_sort(struct type##_static_heap_array *array, bool (*comparison_function)(type *current_element, type *other_element));					\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_map(struct type##_static_heap_array *array, void (*mapping_function)(type *element)); 												\
+size_t type##_static_heap_array_map(struct type##_static_heap_array *array, void (*mapping_function)(type *element)); 													\
 																																										\
 /**  */								 																																	\
-type* type##_static_heap_array_find(struct type##_static_heap_array *array); 																						\
+type* type##_static_heap_array_find(struct type##_static_heap_array *array); 																							\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_last_index_of(struct type##_static_heap_array *array, type const *value); 															\
+size_t type##_static_heap_array_last_index_of(struct type##_static_heap_array *array, type const *value); 																\
 																																										\
 /**  */								 																																	\
-size_t type##_static_heap_array_first_index_of(struct type##_static_heap_array *array, type const *value); 															\
+size_t type##_static_heap_array_first_index_of(struct type##_static_heap_array *array, type const *value); 																\
 																																										\
 /**  */								 																																	\
-struct type##_static_heap_array* type##_static_heap_array_filter_after_copy(struct type##_static_heap_array *array, bool (*filtering_function)(type *element));	\
+struct type##_static_heap_array* type##_static_heap_array_filter_after_copy(struct type##_static_heap_array *array, bool (*filtering_function)(type *element));			\
 																																										\
 /**  */								 																																	\
-void type##_static_heap_array_filter(struct type##_static_heap_array *array, bool (*filtering_function)(type *element)); 											\
+void type##_static_heap_array_filter(struct type##_static_heap_array *array, bool (*filtering_function)(type *element)); 												\
 																																										\
 /**  */								 																																	\
 struct type##_static_heap_array* type##_static_heap_array_reverse(struct type##_static_heap_array *array);
